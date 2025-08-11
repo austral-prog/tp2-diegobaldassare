b10ea6b2923a1c562d2029d75fdd9582ef2f90f2
Diego Baldassare <diegobaldassare@gmail.com>
2025-08-11 17:42:48 -0300

    Hand in

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
+import string
+
+
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.capitalize()} {last_name.capitalize()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+
+ada()
--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("\nVuelto\n")
+    print("Pesos:")
+    print(int(money - expense))
+    print("Centavos:")
+    print(int((money - expense) * 100 % 100))
+change()
+
